005356c506be4712f8ed1b16b6bfe0f00dc9b20b
OVERFORGE <dkdakshkhurana@gmail.com>
2025-09-22 15:13:56 +0530

    marquee fix

--- src/sections/Marquee.jsx
+++ src/sections/Marquee.jsx
@@ -3,11 +3,11 @@ import { motion } from "framer-motion";
 import gifSrc from "../assets/run.gif";
 const Marquee = () => {
   return (
-    <div className="w-full m-auto lg:h-35 h-20 overflow-hidden bg-exebeige">
+    <div className="w-full m-auto lg:h-35 h-21 overflow-hidden bg-exebeige">
       <div className="w-full h-1 bg-exered"></div>
 
       <motion.div
-        initial={{ x: "-100%" }}
+        initial={{ x: "-160%" }}
         animate={{ x: "100%" }}
         transition={{
           duration: 10,
@@ -15,20 +15,20 @@ const Marquee = () => {
           repeatType: "loop",
           ease: "linear",
         }}
-        className="flex h-38 gap-72 -mt-5 "
+        className="flex h-24 lg:h-38 gap-72 -mt-5 "
       >
-        <div className="flex-shrink-0 -mt-5 w-2/3">
+        <div className="flex-shrink-0 lg:-mt-5 -mt-2 w-2/3">
           <img
             src={gifSrc}
             alt="Running GIF"
-            className="h-56 object-cover rounded-lg "
+            className="lg:h-56 h-34  object-cover rounded-lg "
           />
         </div>
-        <div className="flex-shrink-0 -mt-5 w-2/3">
+        <div className="flex-shrink-0 lg:-mt-5 -mt-2 w-2/3">
           <img
             src={gifSrc}
             alt="Running GIF"
-            className="h-56 object-cover rounded-lg "
+            className="lg:h-56 h-34 object-cover rounded-lg "
           />
         </div>
       </motion.div>
